c702bd85f9cc3f658bffeb48ffbaaa4dab50d6e9
NuHazim <nufailhazim.sarifishak@gmail.com>
2025-08-08 08:55:31 +0800

    new file to fetch rows of same name added

---
+++ fillForms.php
@@ -0,0 +1,11 @@
+<?php
+include("database.php");
+
+$name = $_GET['name'] ?? '';
+if ($name) {
+    $stmt = $pdo->prepare("SELECT name,destination,alamat FROM registeredlist WHERE noKenderaan = ?");
+    $stmt->execute([$name]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo json_encode($user);
+}
+?>
