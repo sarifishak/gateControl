7c7bc84c96cec2c3cfa70db2ee153167c2316907
sarifishak <sarif.ishak@gmail.com>
2025-08-08 09:23:15 +0800

    Order by ID desc

--- suggestions.php
+++ suggestions.php
@@ -2,7 +2,7 @@
 include("database.inc");
 $q = $_GET['q'] ?? '';
 if ($q) {
-    $stmt = $pdo->prepare("SELECT noKenderaan FROM registeredlist WHERE noKenderaan LIKE LOWER(?) LIMIT 5");
+    $stmt = $pdo->prepare("SELECT noKenderaan FROM registeredlist WHERE noKenderaan LIKE LOWER(?) ORDER BY id DESC LIMIT 5");
     $stmt->execute([$q . '%']);
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($results);
