42ce00548f4763a396f1e7a2d40da50496352f2f
sarifishak <sarif.ishak@gmail.com>
2025-08-08 09:31:55 +0800

    Update fillForms.php get the latest one

--- fillForms.php
+++ fillForms.php
@@ -1,9 +1,9 @@
-<?php
+BY <?php
 include("database.inc");
 
 $name = $_GET['name'] ?? '';
 if ($name) {
-    $stmt = $pdo->prepare("SELECT name,destination,alamat FROM registeredlist WHERE noKenderaan = ?");
+    $stmt = $pdo->prepare("SELECT name,destination,alamat FROM registeredlist WHERE noKenderaan = ? ORDER BY ID DESC limit 1");
     $stmt->execute([$name]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
     echo json_encode($user);
