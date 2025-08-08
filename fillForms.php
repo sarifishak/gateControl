8f0c5a524619a48d9e7b892fd724fc2eeb944128
sarifishak <sarif.ishak@gmail.com>
2025-08-08 09:48:24 +0800

    Update fillForms.php remove order by id

--- fillForms.php
+++ fillForms.php
@@ -3,7 +3,7 @@ include("database.inc");
 
 $name = $_GET['name'] ?? '';
 if ($name) {
-    $stmt = $pdo->prepare("SELECT name,destination,alamat FROM registeredlist WHERE noKenderaan = ? ORDER BY ID DESC");
+    $stmt = $pdo->prepare("SELECT name,destination,alamat FROM registeredlist WHERE noKenderaan = ?");
     $stmt->execute([$name]);
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
     echo json_encode($user);
